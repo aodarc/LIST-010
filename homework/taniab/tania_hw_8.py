17a2d9aeaa1de9ce2db43968b57d09937ac5265b
Tania Batieva <yalyna.ts@gmail.com>
2018-12-23 20:24:08 +0200

    Added missing code

--- homework/taniab/tania_hw_8.py
+++ homework/taniab/tania_hw_8.py
@@ -1,1 +1,30 @@
 # bubble sort
+test = [{'value': 1625065},
+ {'value': 94},
+ {'value': 59},
+ {'value': 910},
+ {'value': 1385556},
+ {'value': 176},
+ {'value': 347051},
+ {'value': -401731},
+ {'value': -24559}]
+
+
+def bubble(iterable, key=None, reversed=False):
+    lst = list(iterable)
+    key = key if key is not None else lambda elem: elem
+    length = len(lst) - 1
+    is_sorted = False
+
+    while not is_sorted:
+        is_sorted = True
+        for i in range(length):
+            if key(lst[i]) > key(lst[i+1]):
+                is_sorted = False
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+        if reversed:
+            return lst[::-1]
+    return lst
+
+
+print(bubble(test, key=lambda x: x['value']))
