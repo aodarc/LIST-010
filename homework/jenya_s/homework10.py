0fba17088c2f7fef4f45c4243481703a0389ab3b
eugenesamozdran <44786116+eugenesamozdran@users.noreply.github.com>
2018-12-28 16:10:38 +0200

    Update homework10.py

--- homework/jenya_s/homework10.py
+++ homework/jenya_s/homework10.py
@@ -1,30 +1,56 @@
 import requests
 
-def download(url, file_name):
-    # open in binary mode
-    with open(file_name, "wb") as file:
-        # get request
-        response = requests.get(url)
-        # write to file
-        file.write(response.content)
+class ImgDwnld:
 
-IMAGE_FORMATS = ("image/png", "image/jpeg", "image/jpg")
+    IMAGE_FORMATS = ("image/png", "image/jpeg", "image/jpg")
+    DEFAULT_SIZE_LIMIT = 5242880 #this is 5 Mb in bytes
+    
+    def __init__(self, file_path, img_size=DEFAULT_SIZE_LIMIT):
+        self.file_path = file_path
+        self.img_size = img_size
 
-with open("URL_list.txt") as file:
+    def url_list(self):
+        with open(self.file_path) as f:
+            # Making a list where each element is a URL-line from opened file.
+            # Each element except the last one contains '\n' in the end.
 
-    # Making a list where each element is a URL-line from opened file.
-    # Each element except the last one contains '\n' in the end.
+            raw_list_of_urls = f.readlines()
+            clear_list_of_urls = []
 
-    raw_list_of_urls = file.readlines()
-    clear_list_of_urls = []
+            # Getting rid of '\n'
+            for i in raw_list_of_urls:
+                clear_list_of_urls.append(i.replace('\n',''))
 
-    # Getting rid of '\n'
-    for i in raw_list_of_urls:
-        clear_list_of_urls.append(i.replace('\n',''))
+        return clear_list_of_urls
 
-    for url in clear_list_of_urls:
-        r = requests.get(url)
+    def validate_format(self, r):
+        if r.headers["Content-Type"] in self.IMAGE_FORMATS:
+            return True
+        else:
+            print("improper file format: {}".format(r.headers["Content-Type"]))
+            return False
 
-        # checking if URL is an image and its size is less or equal to 5 Mb. If yes - downloading the image
-        if r.headers["Content-Type"] in IMAGE_FORMATS and int(r.headers["Content-Length"]) <= 5242880:
-            download(url, input("file name: "))
+    def validate_size(self, r):
+        if int(r.headers["Content-Length"]) <= self.img_size:
+            return True
+        else:
+            print("the size should be less or equal to {} (in bytes)".format(self.img_size))
+            return False
+
+    def img_download(self, r, img_name):
+        # open in binary mode
+        with open(img_name, "wb") as f:
+            # write to file
+            f.write(r.content)
+
+    def list_download(self):
+        for url in self.url_list():
+            r = requests.get(url)
+            if self.validate_size(r) and self.validate_format(r):
+                self.img_download(r, input("file name: "))
+            else:
+                print("cannot download 1 or more files: {}".format(url))
+
+test = ImgDwnld("URL_list.txt")
+
+test.list_download()
