6eedeb27fa364891354f2991628732ea031c0b2c
eugenesamozdran <44786116+eugenesamozdran@users.noreply.github.com>
2018-12-28 13:50:28 +0200

    Update homework8.py
    fixed key interaction, moved iterable length to separate variable, added error message

--- homework/jenya_s/homework8.py
+++ homework/jenya_s/homework8.py
@@ -4,22 +4,16 @@ def bubble_sort(iter_obj, key=None, reverse=False):
     
     # first, we check if argument is iterable
     # if yes and if it is not 'list', we convert the argument to a list
-    
     if isinstance(iter_obj, Iterable):
 
-        # here we check if some function was passed as a key
-        # and modify our iterable applying the function to all its elements
+        iter_obj = list(iter_obj)
+        key = key if key is not None else lambda x: x
+        length = len(iter_obj)
         
-        if key == None:
-            iter_obj = list(iter_obj)
-            pass
-        else:
-            iter_obj = list(map(key, iter_obj))
-
         # this is the sorting loop itself
-        for i in range(len(iter_obj)):
-            for y in range(len(iter_obj)-i-1):
-                if iter_obj[y] > iter_obj[y+1]:
+        for i in range(length):
+            for y in range(length-i-1):
+                if key(iter_obj[y]) > key(iter_obj[y+1]):
                     iter_obj[y], iter_obj[y+1] = iter_obj[y+1], iter_obj[y]
                     
         # here we check if the result should be reversed or not            
@@ -28,10 +22,8 @@ def bubble_sort(iter_obj, key=None, reverse=False):
         return iter_obj
 
     else:
-        raise TypeError
+        raise TypeError("first position argument expected to be iterable object")
 
 a = [{"value": 42}, {"value": 32}, {"value": 40}, {"value": 56}, {"value": 11}]
-
 a = bubble_sort(a, lambda x: x["value"])
-
 print(a)
