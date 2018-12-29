ab6641b5c798ceb1168073d5321720718c68b4ca
eugenesamozdran <44786116+eugenesamozdran@users.noreply.github.com>
2018-12-29 17:45:46 +0200

    Update homework11.py
    removed property from radius and capacity; calculating it in __init__ block instead depending on arguments passed during instance creation

--- homework/jenya_s/homework11.py
+++ homework/jenya_s/homework11.py
@@ -1,19 +1,14 @@
 from math import pi
 
 class Bubble:
-    def __init__(self, capacity=1):
-        self.capacity = capacity
-        if type(capacity).__name__ not in ("int", "float"):
-            raise ValueError("int or float capacity value is expected")
-
-    #defining functions for sphere radius and square counting
-    #depending on sphere capacity
-    
-    @property
-    def radius(self):
-        radius = ((3*self.capacity)/(4*pi))**(1/3)
-        return radius
-
+    def __init__(self, radius=None, capacity=None):
+        if radius is None:
+            self.capacity = capacity
+            self.radius = ((3*self.capacity)/(4*pi))**(1/3)
+        else:
+            self.radius = radius
+            self.capacity = (4*pi*(self.radius**3))/3
+        
     @property
     def square(self):
         square = 4*pi*(self.radius**2)
@@ -23,24 +18,23 @@ class Bubble:
     #two spheres and returning new sphere object as a result
 
     def __add__(self, other):
-        return Bubble(self.capacity + other.capacity)
+        return Bubble(self.radius + other.radius)
 
     def __sub__(self, other):
-        return Bubble(self.capacity - other.capacity)
+        if self.radius >= other.radius:
+            return Bubble(self.radius - other.radius)
+        else:
+            raise ValueError("Cannot set negative value for a radius")
 
-a = Bubble(5)
-b = Bubble(3)
+a = Bubble(None, 1)
+b = Bubble(None, 4)
 
-print(a.capacity, b.capacity)
 print(a.radius, b.radius)
+print(a.capacity, b.capacity)
 print(a.square, b.square)
 
 c = a+b
 
-print(a.capacity, b.capacity)
-print(a.radius, b.radius)
-print(a.square, b.square)
-
-print(c.capacity)
 print(c.radius)
+print(c.capacity)
 print(c.square)
