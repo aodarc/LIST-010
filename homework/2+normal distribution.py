bb8628d29a545378d52705be92151b4af9e1d4f1
YarynaBrylova <45501160+YarynaBrylova@users.noreply.github.com>
2019-01-13 21:30:50 +0200

    Revert "Added a homework #2"

--- homework/2+normal distribution.py
+++
@@ -1,6 +0,0 @@
-import math
-x = float(input( "Enter parametr  x:"))
-mu = float(input( "Enter parametr  mu:"))
-sigma= float(input( "Enter parametr  sigma:"))
-dist = float(1/(sigma*math.sqrt(2*math.pi)))*math.exp((-(x - mu)**2)/(2*(sigma)**2))
-print(dist)
