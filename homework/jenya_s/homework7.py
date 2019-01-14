532d1cbc7c8676ebbce816542eab03d21ea9100d
eugenesamozdran <44786116+eugenesamozdran@users.noreply.github.com>
2019-01-14 16:51:46 +0200

    Update homework7.py
    used "is" for min and max lim comparison

--- homework/jenya_s/homework7.py
+++ homework/jenya_s/homework7.py
@@ -3,10 +3,10 @@ def magic_range():
     min_lim, max_lim = None, None
 
     for i in range(-200, 1000):
-        if min_lim == None and i is i and i+1 is i+1:
+        if min_lim is None and i is i and i+1 is i+1:
             min_lim = i+1
             
-        if max_lim == None and i+1 is i+1 and i+2 is not i+2:
+        if max_lim is None and i+1 is i+1 and i+2 is not i+2:
             max_lim = i+1
         
     return "[{};{})".format(min_lim, max_lim+1)
