baff2fca05db64ba30d43de3381c14183dda2854
eugenesamozdran <44786116+eugenesamozdran@users.noreply.github.com>
2019-01-24 18:33:12 +0200

    Update homework9.py
    used template :)

--- homework/jenya_s/homework9.py
+++ homework/jenya_s/homework9.py
@@ -1,19 +1,23 @@
 def tag(name):
     def decor(func):
         def wrap(text):
-            if name:
-                name1 = name.lower()
-                print("<{0}>{1}</{0}>".format(name1, func(text)))
-            return func(text)
+            if name.isdigit():
+                return "error"
+            return "<{0}>{1}</{0}>".format(name.lower(), func(text))
         return wrap
     return decor
 
-@tag("SPAN")
-def foo(text):
-    try:
-        return text.upper()
-    except AttributeError:
-        print("Please use strings as foo function arguments only")
-        raise
 
-foo([1])
+TEST_CASES = [
+		({'name':'span'}, 'lorem ipsum', '<span>LOREM IPSUM</span>'),
+		({'name':'div'}, 'lorem ipsum', '<div>LOREM IPSUM</div>'),
+		({'name':'STRONG'}, 'lorem ipsum', '<strong>LOREM IPSUM</strong>'),
+		({'name':'123'}, 'lorem ipsum', 'error'),
+	]
+
+for case in TEST_CASES:
+	@tag(**case[0])
+	def foo(text):
+		return text.upper()
+
+	assert foo(case[1]) == case[2]
