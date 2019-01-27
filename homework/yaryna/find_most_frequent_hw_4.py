7e9a22b2a42069443c391e786e157fa32441b507
YarynaBryliova <yaryna.bryliova@gmail.com>
2019-01-28 00:15:54 +0200

    Updated after review #2

--- homework/yaryna/find_most_frequent_hw_4.py
+++ homework/yaryna/find_most_frequent_hw_4.py
@@ -1,14 +1,13 @@
 string = "Mom! Mom! Are you sleeping?! you!!"
 
-string = string.lower()
-string = string.replace("-"," ").replace(",","").replace("!","").replace("?","").replace(".","")
-
-def most_frequent(list_of_string):
+def most_frequent(string):
+    string = string.lower()
+    string = string.replace("-"," ").replace(",","").replace("!","").replace("?","").replace(".","")
+    list_of_string = string.split(" ")
     d = {item:list_of_string.count(item) for item in list_of_string }
 
     max_value = max(d.values())
     most_frequent_word = sorted(key for key in d if d[key] == max_value)
     print(most_frequent_word )
 
-list_of_string = string.split(" ")
-most_frequent(list_of_string)
+most_frequent(string)
