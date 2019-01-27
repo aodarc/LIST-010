4c91ff68d52ad4b0418bd2333955a46657173bac
YarynaBryliova <yaryna.bryliova@gmail.com>
2019-01-27 23:30:21 +0200

    Added a homework #1

---
+++ homework/yaryna/game_hw_1.py
@@ -0,0 +1,197 @@
+print("Hello! This game is about emotions. Try to put letters in the correct order and create words")
+
+n=0
+
+word = "happy"
+print("ypahp")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+word = "exited"
+print("txidee")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+word = "lucky"
+print("cyulk")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+word = "angry"
+print("ynrga")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+word = "careful"
+print("lfarcue")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+word = "suprised"
+print("priesdur")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+word = "glad"
+print("lgda")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+word = "sleepy"
+print("ypeels")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+word = "shocked"
+print("edcoshk")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+word = "upset"
+print("utsep")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+
+word = "worried"
+print("ordiwer")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+
+word = "unpleasant"
+print("taalnupesn")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+
+word = "pessimistic"
+print("esmiiisstcp")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+
+word = "unhappy"
+print("hyaupnp")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+
+word = "sorry"
+print("rysor")
+for i in range(1,3):
+    word1= input("Enter your answer:")
+    if word1==word:
+        print("Great!")
+        n+=1
+        break
+    else:
+        print("Try again!")
+        i=i+1
+
+
+
+if n >= 13:
+    print("Congratulations! Yor result is {} !".format(n))
+elif n >= 9 and n < 13:
+    print("It was great! Your result is  {} !".format (n))
+else:
+    print("You need be more careful!  Your result is {} !".format(n))
