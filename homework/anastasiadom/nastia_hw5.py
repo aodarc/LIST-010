6b2b6c686fdbc12b5ed046bfd0c94248d3ec8595
Anastasiia Domashovets <nastiadom97@gmail.com>
2019-02-04 00:14:31 +0200

    Homework #5: Polindrom (Fixed)

--- homework/anastasiadom/nastia_hw5.py
+++ homework/anastasiadom/nastia_hw5.py
@@ -1,10 +1,8 @@
 import re
 import sys
 
-def StrPolindrom(s):
-	s1=s.lower()
-	s2=re.sub(r'\s+','',s1)
-	s3=s2[::-1]
-	return 'YES' if s3==s2 else 'NO'
+def strPolindrom(inputValue):
+	inputValue=re.sub(r'\s+','', inputValue.lower())
+	return 'YES' if inputValue==inputValue[::-1] else 'NO'
 
-print (StrPolindrom(sys.argv[1]))
+print (strPolindrom(sys.argv[1]))
