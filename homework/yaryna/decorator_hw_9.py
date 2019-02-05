f4c60fd71c6ba9ad7673f69a29c35d45b336ee5f
YarynaBryliova <yaryna.bryliova@gmail.com>
2019-02-05 23:05:04 +0200

    Refactored after code review

--- homework/yaryna/decorator_hw_9.py
+++ homework/yaryna/decorator_hw_9.py
@@ -1,19 +1,40 @@
-name = input("Enter decorator name: ")
-text = input("Enter text for function: ")
+#name = input("Enter decorator name: ")
+#text = input("Enter text for function: ")
 
-def decorator(foo,name = name):
-    def wrapper(text):
-        return (f"<{name.lower()}>" f"{foo(text)}" f"</{name.lower()}>")
-    return wrapper
+def tag(name):
+    def decorator(foo):
+        def wrapper(text):
+            if name.isdigit():   
+                return "Error"
+            return (f"<{name.lower()}>" f"{foo(text)}" f"</{name.lower()}>")
+        return wrapper
+    return decorator
 
-if name.isalpha() is True:
-    @decorator
+
+# @decorato
+# def foo(text):
+#     if name.isalpha():
+#         return (text.upper())
+#     else:
+#         print("Error")
+
+TEST_CASES = [
+		({'name':'span'}, 'lorem ipsum', '<span>LOREM IPSUM</span>'),
+		({'name':'div'}, 'lorem ipsum', '<div>LOREM IPSUM</div>'),
+		({'name':'STRONG'}, 'lorem ipsum', '<strong>LOREM IPSUM</strong>'),
+		({'name':'123'}, 'lorem ipsum', 'error'),
+	]
+
+for case in TEST_CASES:
+    @tag(**case[0])
     def foo(text):
-        return (text.upper())
-else:
-    print("Error")
+            return text.upper()
+		
 
-print(foo(text))
+    assert foo(case[1]) == case[2], 'My error'
+
+
+
 
 
 
