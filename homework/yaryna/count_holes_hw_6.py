aa459151a7c1bcb4f6299fde55c751b32a4f9599
YarynaBryliova <yaryna.bryliova@gmail.com>
2019-02-05 23:50:32 +0200

    Refactored after code review

--- homework/yaryna/count_holes_hw_6.py
+++ homework/yaryna/count_holes_hw_6.py
@@ -3,7 +3,7 @@ string = str(input("Enter sequence:"))
 def count_holes(string):
     holes = {"0":1,"4":1,"6":1,"8":2,"9":1}
     string = string.replace("-","")
-    if string.isdigit()==True:
+    if string.isdigit():
         list_of_string = list(string.lstrip('0'))
         res = 0
         for element in list_of_string:
