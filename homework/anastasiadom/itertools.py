1cb72ef6867be694c9c90e0d8796eceb33d84ed5
Anastasiia Domashovets <nastiadom97@gmail.com>
2019-02-05 23:57:19 +0200

    Homework: Itertools (Fixed)

--- homework/anastasiadom/itertools.py
+++ homework/anastasiadom/itertools.py
@@ -4,19 +4,23 @@ from operator import mul
 
 class Tools:
 
-	def reduceStaff(self, lst = []):
+	def reduceStaff(self, lst = None):
+		lst = lst or []
 		product = reduce((lambda x, y: x*y), lst)
 		print(product)
 
-	def filterStaff(self, lst = []):
+	def filterStaff(self, lst = None):
+		lst = lst or []
 		grater_than_five = list(filter(lambda x: x>5, lst))
 		print(grater_than_five)
 
-	def mapStaff(self, lst = []):
+	def mapStaff(self, lst = None):
+		lst = lst or []
 		squared = list(map(lambda x: x**2, lst))
 		print(squared)
 
-	def accumulateStaff(self, lst = []):
+	def accumulateStaff(self, lst = None):
+		lst = lst or []
 		mulv = list(accumulate(lst, mul))
 		print(mulv)
 
